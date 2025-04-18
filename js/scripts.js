722ecc147922e0ad0265e10416aaf9c6abaff96a
souravdas090300 <souravdas090300@gmail.com>
2025-04-18 19:32:39 +0200

    Updated scripts.js
    I have deleted some console.log because of task.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 let pokemonList = [
-  { name: "Balbassure", height: 7, type: ["grass", "Poison"] },
+  { name: "Bulbasaur", height: 7, type: ["Grass", "Poison"] },
   { name: "Charizard", height: 17, type: ["Fire", "Flying"] },
-  { name: "Blasttoise", height: 16, type: ["Water"] },
+  { name: "Blastoise", height: 16, type: ["Water"] },
 ];
-console.log(pokemonList);

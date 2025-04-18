cdf9b68c6524c0bd62a6d49772579d751506034b
souravdas090300 <souravdas090300@gmail.com>
2025-04-18 19:20:49 +0200

    Updated scripts.js
    I have created some complex variables in JavaScript, whose object is the Pokémon 3 content in an Array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert("Hello world");
-let favoriteFood = "Pizza";
-document.write(favoriteFood);
+let pokemonList = [
+  { name: "Balbassure", height: 7, type: ["grass", "Poison"] },
+  { name: "Charizard", height: 17, type: ["Fire", "Flying"] },
+  { name: "Blasttoise", height: 16, type: ["Water"] },
+];
+console.log(pokemonList);

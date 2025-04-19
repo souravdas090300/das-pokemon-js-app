1a7b70c89da3bf9e41b2fda603e5e7330d43376b
souravdas090300 <souravdas090300@gmail.com>
2025-04-19 15:25:10 +0200

    Updated scripts.js
    I have added Pokémon code, which is js loop and conditional,

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,29 @@
 let pokemonList = [
-  { name: "Bulbasaur", height: 7, type: ["Grass", "Poison"] },
-  { name: "Charizard", height: 17, type: ["Fire", "Flying"] },
-  { name: "Blastoise", height: 16, type: ["Water"] },
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    type: ["Grass", "Poison"],
+  },
+  {
+    name: "Charizard",
+    height: 1.7,
+    comment: "Wow that's big!",
+    type: ["Fire", "Flying"],
+  },
+  {
+    name: "Blastoise",
+    height: 1.6,
+    type: ["Water"],
+  },
 ];
+
+let output = "";
+for (let i = 0; i < pokemonList.length; i++) {
+  output += `${pokemonList[i].name} (Height: ${pokemonList[i].height})`;
+  if (pokemonList[i].comment) {
+    output += ` - ${pokemonList[i].comment}`;
+  }
+  output += `<br>`;
+}
+
+document.body.innerHTML = output;

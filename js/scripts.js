6cd984c6658b1485810b58815ffd34ed204ba78d
souravdas090300 <souravdas090300@gmail.com>
2025-04-24 15:30:51 +0200

    Updated css,js,html
    I have added some code on scripts.js, index.html, and styles.css. I have used new function on my js app.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
-// Wrapping the pokemonList in an IIFE and creating pokemonRepository
+// Wrapping the pokemonList in an IIFE and creating the pokemonRepository object
 let pokemonRepository = (function () {
   // This array is now private to the IIFE
   let pokemonList = [
     { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
     { name: "Charizard", height: 1.7, types: ["fire", "flying"] },
     { name: "Blastoise", height: 1.6, types: ["water"] },
+    { name: "pikachu", height: 0.4, types: ["electric"] },
   ];
 
   // Public functions that will be returned
@@ -15,24 +16,24 @@ let pokemonRepository = (function () {
   function add(pokemon) {
     pokemonList.push(pokemon);
   }
-  pokemonRepository.getAll().forEach(function (pokemon) {
-    let pokemonList = document.querySelector(".pokemon-list"); // select the ul elemennt
-    let listpokemon = document.createElement("li"); // create a new li element
-    let button = document.createElement("button"); // ceate a new button element
-    button.innerText = "pokemon.name"; // set the button's text to the pokemon's name
-    button.classList.add("button-class");
-    listpokemon.appendChild(button);
-    pokemonList.appendChild(listpokemon); // append the li element to the ul
-    but;
-  });
+
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+
   function addListItem(pokemon) {
-    let ulElement = document.querySelector("ul"); // Select the ul element
-    let liElement = document.createElement("li"); // Create a new li element
-    let button = document.createElement("button"); // Create a new button element
-    button.innerText = pokemon.name; // Set the button's text to the Pokémon's name
-    button.classList.add("pokemon-button"); // Add a class to the button
-    liElement.appendChild(button); // Append the button to the li element
-    ulElement.appendChild(liElement); // Append the li element to the ul
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("pokemon-list");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+
+    // Add event listener to the button
+    button.addEventListener("click", function () {
+      showDetails(pokemon);
+    });
   }
 
   // Return an object with the public functions
@@ -42,9 +43,6 @@ let pokemonRepository = (function () {
     addListItem: addListItem,
   };
 })();
-
-// Add a new Pokémon to the repository
-pokemonRepository.add({ name: "Pikachu", height: 0.4, types: ["electric"] });
 
 // Iterate over each Pokémon in the repository
 pokemonRepository.getAll().forEach(function (pokemon) {

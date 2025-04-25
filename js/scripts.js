b1d1473b77cd35e4ba5790ec08962e30ae31d418
souravdas090300 <souravdas090300@gmail.com>
2025-04-25 17:43:41 +0200

    Updated scripts.js, cretaed fetch-polyfills, created promise-polyfill.js
    In this commit, I have added two new things, promise and fetch polyfill, which are supported by old and new browsers, and also created a pokemon  150 name list with details

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,24 @@
 // Wrapping the pokemonList in an IIFE and creating the pokemonRepository object
 let pokemonRepository = (function () {
   // This array is now private to the IIFE
-  let pokemonList = [
-    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
-    { name: "Charizard", height: 1.7, types: ["fire", "flying"] },
-    { name: "Blastoise", height: 1.6, types: ["water"] },
-    { name: "pikachu", height: 0.4, types: ["electric"] },
-  ];
+  let pokemonList = [];
+  let apiurl = "https://pokeapi.co/api/v2/pokemon/?offset=150&limit=150";
+
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "detailsurl" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.error("Invalid Pokémon object");
+    }
+  }
 
   // Public functions that will be returned
   function getAll() {
     return pokemonList;
-  }
-
-  function add(pokemon) {
-    pokemonList.push(pokemon);
   }
 
   function showDetails(pokemon) {
@@ -22,17 +26,60 @@ let pokemonRepository = (function () {
   }
 
   function addListItem(pokemon) {
-    let pokemonList = document.querySelector(".pokemon-list");
+    let pokemonListElement = document.querySelector(".pokemon-list");
     let listpokemon = document.createElement("li");
     let button = document.createElement("button");
     button.innerText = pokemon.name;
     button.classList.add("pokemon-item");
     listpokemon.appendChild(button);
-    pokemonList.appendChild(listpokemon);
+    pokemonListElement.appendChild(listpokemon);
 
     // Add event listener to the button
     button.addEventListener("click", function () {
       showDetails(pokemon);
+    });
+  }
+
+  function loadList() {
+    return fetch(apiurl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsurl: item.url,
+          };
+          add(pokemon);
+          console.log(pokemon);
+        });
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsurl;
+    return fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (details) {
+        // Add the details to the item
+        item.imageurl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
+
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
     });
   }
 
@@ -41,10 +88,15 @@ let pokemonRepository = (function () {
     getAll: getAll,
     add: add,
     addListItem: addListItem,
+    showDetails: showDetails,
+    loadList: loadList,
+    loadDetails: loadDetails,
   };
 })();
 
-// Iterate over each Pokémon in the repository
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+// Load the Pokémon list and display it
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
